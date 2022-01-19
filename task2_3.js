e80ef50854b86dd793a7ddd8f6304bf913b69b48
AndrejKost <vi060480kav@gmail.com>
2022-01-19 23:23:59 +0200

    задача 3

---
+++ task2_3.js
@@ -0,0 +1,10 @@
+function f(a,b,c) {
+    if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number') {
+        var res = (a + b) / c;
+        console.log(res);
+    } else {
+        throw new Error('all parameters type should be a Number');
+    }
+};
+f(9,3,2);
+f(1,'s',1);
