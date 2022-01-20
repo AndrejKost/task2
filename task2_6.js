52be0a4d0faffd7b85ae44ef2e66ad222f711458
AndrejKost <vi060480kav@gmail.com>
2022-01-20 22:52:59 +0200

    задача 5

---
+++ task2_6.js
@@ -0,0 +1,8 @@
+function isEven(n) {
+    if ( typeof n === 'number') {
+        return (n % 2 === 0);
+    } else {
+        throw new Error('parameter type is not a Number');
+    }
+};
+console.log(isEven(''));
